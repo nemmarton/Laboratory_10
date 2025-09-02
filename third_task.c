bf407eb6f8d1d0c7e1f1efc9ee1d5a48fba3f1ea
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-09-02 11:03:19 +0200

    Update third_task.c

--- third_task.c
+++ third_task.c
@@ -1,13 +1,30 @@
 /* Task description:
-
+Test the following two functions. What is the difference between the two functions? What is the difference between the outputs? (putchar(c) prints a single character like printf("%c",c) would do.) 
 
 */
 
 
 #include <stdio.h>
 
-int main(){
+void print_string_1(char *text) {
+    if (text[0] == '\0')
+        return;
+    putchar(text[0]);
+    printf("%s", text + 1);     // !
+}
 
 
-  return 0;
+void print_string_2(char *text) {
+    if (text[0] == '\0')
+        return;
+    putchar(text[0]);
+    print_string_2(text + 1); // !
+}
+
+
+int main(void) {
+    print_string_1("word");
+    print_string_2("word");
+    
+    return 0;
 }
