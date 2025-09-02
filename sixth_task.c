8a4bb0297bef00d2e196db13e54bba5287d6ea12
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-09-02 11:04:58 +0200

    Update sixth_task.c

--- sixth_task.c
+++ sixth_task.c
@@ -1,1 +1,20 @@
+/* Task description:
+Pretty print numbers
 
+Write a function that prints the positive integer received as a parameter in an easy to read format: form groups of three digits and separate them by spaces. 
+Example: 16 077 216. Test for other values, too: 999, 1000, 12, 0, 1000222.
+
+Hint:
+Use recursion, it is the same as printing in base 1000 numeral system.
+To printf 3 digits with leading zeros use %03d in the format string.
+
+*/
+
+
+#include <stdio.h>
+
+int main(){
+
+
+  return 0;
+}
