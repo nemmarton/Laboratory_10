7f7c67b1691bc5f2232216d0c138a1f57094bfe8
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-09-02 11:02:35 +0200

    Update second_task.c

--- second_task.c
+++ second_task.c
@@ -1,5 +1,10 @@
 /* Task description:
+Fibonacci
 
+The Fibonacci-series is defined as: F0=0, F1=1, Fn=Fn-1+Fn-2. Write a recursive function to calculate the nth element! Test your function for n=40! What happens? (Hint: remember the lecture topic.)
+
+Trace your program in the debugger. Start it and see how it works for n=5. You may implement your own tracing method: your fib() function should first print the value of the parameter (n).
+Use the debugger to trace the stack!
 
 */
 
