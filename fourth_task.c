3c2b136a33c4520f7753af7e2116515310f42cc5
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-09-02 11:03:40 +0200

    Update fourth_task.c

--- fourth_task.c
+++ fourth_task.c
@@ -1,5 +1,6 @@
 /* Task description:
-
+Write a recursive function to print the elements of an array a) forwards b) backwards. Both functions should receive the array and its size on the parameter list. 
+Create in main an array of five, and another one of ten integers (initialized). Call both functions for both arrays.
 
 */
 
