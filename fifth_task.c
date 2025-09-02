00b05e88ca128803ec292c5a7201c40eb3d69806
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-09-02 11:04:00 +0200

    Update fifth_task.c

--- fifth_task.c
+++ fifth_task.c
@@ -1,4 +1,8 @@
 /* Task description:
+Write a function that takes two parameters: a positive integer value, and the base of a numeral system. Print the value in the given numeral system. Why is the solution much simpler using recursion than an iterative solution?
+Hint
+
+To print for example 1234 in base 10 numeral system first the 1234/10 (123) should be printed, and then the 1234%10 (4). Using recursion it is easy to reverse the order of printing.
 
 
 */
